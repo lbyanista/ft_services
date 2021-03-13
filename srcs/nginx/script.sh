c8e61fb1817440f13822f7ba6f4e468645060380
Mohamed fadel Labrayji <mlabrayj@e2r4p12.1337.ma>
2021-03-13 12:29:11 +0100

    addwordpress

--- srcs/nginx/script.sh
+++ srcs/nginx/script.sh
@@ -1,6 +1,13 @@
-apk update
-apk add nginx openrc --no-cache
-rc-status
-touch touch /run/openrc/softlevel
-echo "daemon off;" >> /etc/nginx/nginx.conf
+# apk update
+# apk add vim nginx openrc --no-cache
+# rc-status
+# touch /run/openrc/softlevel
+#echo "daemon off;" >> /etc/nginx/nginx.conf
+
+sudo openrc reboot
+
+
 rc-service nginx start
+
+
+rc-service php-fpm7 start

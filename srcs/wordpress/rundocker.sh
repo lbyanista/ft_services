68b0ac957199f707939f60f91557bf081634a95a
Mohamed fadel Labrayji <mlabrayj@e2r4p12.1337.ma>
2021-03-31 11:32:19 +0100

    wpmk4ip

--- srcs/wordpress/rundocker.sh
+++ srcs/wordpress/rundocker.sh
@@ -1,2 +1,5 @@
+MK4IP=$(minikube ip)
+echo "minikube ip is $MK4IP"
+sed -i '' "s/MK4IP/$MK4IP/g" wordpress.sql
 docker build -t alpine-wordpress . --no-cache
 docker run -it -p 5050:5050 alpine-wordpress
